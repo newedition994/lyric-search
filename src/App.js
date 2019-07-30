4474b6bb5e299e9f7545c40e6fd62eee8c4c64e2
Breyanna Vass <breyannavass@gmail.com>
2019-07-30 13:01:05 -0400

    Adding Context and Providers

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import Navbar from './components/layout/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Index from './components/layout/Index'
 
+import { Provider } from './context'
+
 function App() {
   return (
-    <Router>
-      <React.Fragment>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Index} />
-          </Switch>
-        </div>
-      </React.Fragment>
-    </Router>
+    <Provider>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ src/components/tracks/Tracks.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Consumer } from '../../context'
+
+class Tracks extends Component {
+    render() {
+        return (
+            <Consumer>
+                {value => {
+                    console.log(value);
+                    return <h1>Tracks</h1>
+                }}
+            </Consumer>
+        )
+    }
+}
+
+export default Tracks
