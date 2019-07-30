20be0635d0be22447be8cee2f82d02c21b18344c
Breyanna Vass <breyannavass@gmail.com>
2019-07-30 12:47:26 -0400

    Adding Tracks to the application

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Index from './components/layout/Index'
 
 function App() {
   return (
@@ -10,9 +11,7 @@ function App() {
         <Navbar />
         <div className="container">
           <Switch>
-            <Route>
-
-            </Route>
+            <Route exact path="/" component={Index} />
           </Switch>
         </div>
       </React.Fragment>
