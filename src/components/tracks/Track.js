86aa7f417fe86ee131947da60c4b2802bbae98fb
Breyanna Vass <breyannavass@gmail.com>
2019-08-07 11:58:41 -0400

    Adding track to components

---
+++ src/components/tracks/Track.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Track = (props) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Track;
--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Consumer } from '../../context'
 import Spinner from '../layout/Spinner'
+import Track from '../tracks/Track'
 
 class Tracks extends Component {
     render() {
@@ -8,11 +9,20 @@ class Tracks extends Component {
             <Consumer>
                 {value => {
                     console.log(value);
-                    const { track_list } = value;
+                    const { track_list, heading } = value;
                     if (track_list === undefined || track_list.length === 0) {
                         return <Spinner />
                     } else {
-                        return <h1>Tracks Loaded</h1>
+                        return (
+                            <React.Fragment>
+                                <h3 className="text-center mb-4">{heading}</h3>
+                                <div className="row">
+                                    {track_list.map(item =>
+                                        <Track key={item.track.track_id} track={item.track} />
+                                    )}
+                                </div>
+                            </React.Fragment>
+                        )
                     }
                 }}
             </Consumer>
