f73843bc00a433516abd1236abb5e948e39252f0
Breyanna Vass <breyannavass@gmail.com>
2019-08-30 10:12:17 -0400

    Revised lyric search

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/layout/Navbar';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Index from './components/layout/Index'
-import Lyrics from './components/tracks/Lyrics'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import { Provider } from './context'
+import Navbar from "./components/layout/Navbar";
+import Index from "./components/layout/Index";
+import Lyrics from "./components/tracks/Lyrics";
 
-function App() {
+import "./App.css";
+
+import { ContextController } from "./context";
+
+const App = () => {
   return (
-    <Provider>
+    <ContextController>
       <Router>
-        <React.Fragment>
+        <>
           <Navbar />
           <div className="container">
             <Switch>
               <Route exact path="/" component={Index} />
-              <Route exact path="/lyrics/tracks/:id" component={Lyrics} />
+              <Route exact path="/lyrics/track/:id" component={Lyrics} />
             </Switch>
           </div>
-        </React.Fragment>
+        </>
       </Router>
-    </Provider>
+    </ContextController>
   );
-}
+};
 
 export default App;
--- src/context.js
+++ src/context.js
@@ -1,31 +1,48 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Context = React.createContext();
+export const Context = React.createContext();
 
-export class Provider extends Component {
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case 'SEARCH_TRACKS':
+//       return {
+//         ...state,
+//         track_list: action.payload,
+//         heading: 'Search Results'
+//       };
+//     default:
+//       return state;
+//   }
+// };
 
-    state = {
+export function ContextController({ children }) {
+    let intialState = {
         track_list: [],
-        heading: 'Top 10 tracks'
-    }
+        heading: ""
+        // dispatch: action => this.setState(state => reducer(state, action))
+    };
 
-    componentDidMount() {
-        axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
+    const [state, setState] = useState(intialState);
+
+    useEffect(() => {
+        axios
+            .get(
+                `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
+                process.env.REACT_APP_MM_KEY
+                }`
+            )
             .then(res => {
-                // console.log(res.data)
-                this.setState({ track_list: res.data.message.body.track_list })
+                // console.log(res.data);
+                setState({
+                    track_list: res.data.message.body.track_list,
+                    heading: "Top 10 Tracks"
+                });
             })
-            .catch(err => console.log(err))
-    }
+            .catch(err => console.log(err));
+    }, []);
 
-    render() {
-        return (
-            <Context.Provider value={this.state}>
-                {this.props.children}
-            </Context.Provider>
-        )
-    }
+    return (
+        <Context.Provider value={[state, setState]}>{children}</Context.Provider>
+    );
 }
-
-export const Consumer = Context.Consumer;
